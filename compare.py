d1521d802833c1c421a2b8108f2819ba998e877d
pgivens4915 <pgivens4915@gmail.com>
2013-11-20 12:11:04 -0700

    I made compare more intuitive

--- compare.py
+++ compare.py
@@ -7,11 +7,10 @@ inputFileTwo = sys.argv[2]
 targetStart  = int(sys.argv[3])
 targetStop   = int(sys.argv[4])
 
+targetStart = targetStart - 1
+
 fileOne = open(inputFileOne, 'r')
 fileTwo = open(inputFileTwo, 'r')
-
-print fileOne
-print fileTwo
 
 # Done three times to get to the correct line
 stringOne = fileOne.readline()
@@ -20,9 +19,6 @@ stringOne = fileOne.readline()
 stringTwo = fileTwo.readline()
 stringOne = list(fileOne.readline())
 stringTwo = list(fileTwo.readline())
-
-print "".join(stringOne)
-print "".join(stringTwo)
 
 targetOne = stringOne[targetStart : targetStop]
 targetTwo = stringTwo[targetStart : targetStop]
